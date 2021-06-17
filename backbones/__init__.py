04f2c954f88c1e8a5b267c85b6d742e75d828ec6
Patrick Deubel <patrick.deubel@gmail.com>
2021-06-17 12:08:36 +0200

    Added a configuration

---
+++ backbones/__init__.py
@@ -0,0 +1,5 @@
+from .resnet18 import ResNet18
+
+backbone_models = {
+    "resnet18": ResNet18
+}
---
+++ datasets/__init__.py
@@ -0,0 +1,2 @@
+from .concrete_cracks import ConcreteCracksDataset
+from .sdnet2018 import SDNet2018
