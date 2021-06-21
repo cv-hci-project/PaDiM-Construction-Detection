d0cd42b07e3b347869a61268d415b4f193dad979
Patrick Deubel <patrick.deubel@gmail.com>
2021-06-21 12:57:30 +0200

    PaDiM learned means and covariances were not saved due to a bug, now fixed

--- models/padim.py
+++ models/padim.py
@@ -2,7 +2,7 @@ import torch
 from torch.nn import Module, Parameter
 
 from backbones import backbone_models
-from utils.utils import get_embedding
+from utils.utils import get_embedding, calculate_score_map
 
 
 class PaDiM(Module):
@@ -34,40 +34,48 @@ class PaDiM(Module):
 
         self.n = 0
 
-        self.calculated_means = None
-        self.calculated_covariances = None
+        self.learned_means = Parameter(
+            torch.zeros((self.number_of_patches, self.number_of_embeddings)),
+            requires_grad=False
+        ).to(self.device)
+
+        self.learned_covariances = Parameter(
+            torch.zeros((self.number_of_patches, self.number_of_embeddings, self.number_of_embeddings)),
+            requires_grad=False
+        ).to(self.device)
+
+        self.crop_size = params["crop_size"]
 
     def calculate_means_and_covariances(self):
-        self.calculated_means = self.means.clone().to(self.device)
-        self.calculated_covariances = self.covariances.clone().to(self.device)
+        self.learned_means = Parameter(self.means.clone(), requires_grad=False).to(self.device)
+        self.learned_covariances = Parameter(self.covariances.clone(), requires_grad=False).to(self.device)
 
         epsilon = 0.01
 
         identity = torch.eye(self.number_of_embeddings).to(self.device)
-        self.calculated_means /= self.n
+        self.learned_means /= self.n
 
         for i in range(self.number_of_patches):
-            self.calculated_covariances[i, :, :] -= self.n * torch.outer(self.calculated_means[i, :],
-                                                                                 self.calculated_means[i, :])
-            self.calculated_covariances[i, :, :] /= self.n - 1  # corrected covariance
-            self.calculated_covariances[i, :, :] += epsilon * identity  # constant term
+            self.learned_covariances[i, :, :] -= self.n * torch.outer(self.learned_means[i, :],
+                                                                      self.learned_means[i, :])
+            self.learned_covariances[i, :, :] /= self.n - 1  # corrected covariance
+            self.learned_covariances[i, :, :] += epsilon * identity  # constant term
 
         # TODO we could delete self.means and self.covariances as they are not needed anymore (they are only running
         #  variables to track the batches)
 
     def forward(self, x):
-        # TODO inference step here, i.e. calculating distsance function
         x = x.to(self.device)
 
+        with torch.no_grad():
+            features_1, features_2, features_3 = self.backbone(x)
+
+        embeddings = get_embedding(features_1, features_2, features_3, self.embedding_ids, self.device)
+        b, c, h, w = embeddings.size()
+
+        embeddings = embeddings.view(-1, self.number_of_embeddings, self.number_of_patches)
+
         if self.training:
-            with torch.no_grad():
-                features_1, features_2, features_3 = self.backbone(x)
-
-            embeddings = get_embedding(features_1, features_2, features_3, self.embedding_ids)
-            b, c, h, w = embeddings.size()
-
-            embeddings = embeddings.view(-1, self.number_of_embeddings, self.number_of_patches)
-
             for i in range(self.number_of_patches):
                 patch_embeddings = embeddings[:, :, i]  # b * c
                 for j in range(b):
@@ -78,4 +86,6 @@ class PaDiM(Module):
             self.n += b  # number of images
         else:
             # TODO calculate distance map here
-            raise NotImplementedError()
+            scores = calculate_score_map(embeddings, (b, c, h, w), self.learned_means,
+                                         self.learned_covariances, self.crop_size, min_max_norm=True)
+            return torch.Tensor(scores)
