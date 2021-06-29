b1979b477a96e5d59ee8785be654fde8a1c19c03
Patrick Deubel <patrick.deubel@gmail.com>
2021-06-29 22:04:35 +0200

    Added VanillaVAESecond as a backbone

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -1,15 +1,18 @@
 from .resnet18 import ResNet18
 from .wide_resnet50 import WideResNet50
 from .vanilla_vae import VanillaVAE
+from .vanilla_vae_second import VanillaVAESecond
 
 backbone_models = {
     "resnet18": ResNet18,
     "wide_resnet50": WideResNet50,
-    "vanilla_vae": VanillaVAE
+    "vanilla_vae": VanillaVAE,
+    "vanilla_vae_second": VanillaVAESecond
 }
 
 backbone_kinds = {
     "resnet18": "pretrained_imagenet",
     "wide_resnet50": "pretrained_imagenet",
-    "vanilla_vae": "vae"
+    "vanilla_vae": "vae",
+    "vanilla_vae_second": "vae"
 }
---
+++ backbones/vanilla_vae_second.py
@@ -0,0 +1,86 @@
+from typing import List
+
+from torch import Tensor
+from torch import nn
+
+
+class VanillaVAESecond(nn.Module):
+
+    def __init__(self,
+                 in_channels: int,
+                 latent_dim: int,
+                 hidden_dims: List,
+                 **kwargs):
+        super().__init__()
+
+        self.latent_dim = latent_dim
+
+        modules = []
+        if hidden_dims is None:
+            hidden_dims = [32, 64, 128, 256, 512]
+
+        _in_channels = in_channels
+
+        # Build Encoder
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(_in_channels, out_channels=h_dim,
+                              kernel_size=3, stride=1, padding=1),
+                    nn.MaxPool2d(2),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            _in_channels = h_dim
+
+        self.encoder = nn.Sequential(*modules)
+        # TODO I hacked this hardcoded value into here because original author did the same, should be better done
+        self.fc_mu = nn.Linear(hidden_dims[-1] * 4, latent_dim)
+        self.fc_var = nn.Linear(hidden_dims[-1] * 4, latent_dim)
+
+        # Build Decoder
+        modules = []
+
+        # TODO Same as above, redo the hardcoded stuff
+        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * 4)
+
+        hidden_dims.reverse()
+
+        for i in range(len(hidden_dims) - 1):
+            modules.append(
+                nn.Sequential(
+                    nn.ConvTranspose2d(hidden_dims[i],
+                                       hidden_dims[i + 1],
+                                       kernel_size=2,
+                                       stride=2,
+                                       # padding=1,
+                                       # output_padding=1
+                                       ),
+                    nn.BatchNorm2d(hidden_dims[i + 1]),
+                    nn.LeakyReLU())
+            )
+
+        self.decoder = nn.Sequential(*modules)
+
+        self.final_layer = nn.Sequential(
+            nn.ConvTranspose2d(hidden_dims[-1],
+                               hidden_dims[-1],
+                               kernel_size=2,
+                               stride=2,
+                               # padding=1,
+                               # output_padding=1
+                               ),
+            nn.BatchNorm2d(hidden_dims[-1]),
+            nn.LeakyReLU(),
+            nn.Conv2d(hidden_dims[-1], out_channels=3,
+                      kernel_size=3, stride=1, padding=1),
+            nn.Tanh())
+
+    def forward(self, x: Tensor):
+
+        x = self.encoder[0](x)
+        feature_1 = self.encoder[1](x)
+        feature_2 = self.encoder[2](feature_1)
+        feature_3 = self.encoder[3](feature_2)
+
+        return feature_1, feature_2, feature_3
