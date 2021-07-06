f6372a6b1b1c1c0c78a2a104249b81b0560434ec
Lei_Wan <jonaswanlei00@gmail.com>
2021-07-06 13:25:08 +0200

    add backbone

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -1,7 +1,9 @@
 from .resnet18 import ResNet18
 from .wide_resnet50 import WideResNet50
+from .efficientnet import EffNet
 
 backbone_models = {
     "resnet18": ResNet18,
-    "wide_resnet50": WideResNet50
+    "wide_resnet50": WideResNet50,
+    "efficientnet": EffNet
 }
